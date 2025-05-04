ccfa47423da3243b8de672b56f45ed3b9894cb0f
harikrishnab438 <harikrishnab438@gmail.com>
2025-05-04 11:13:14 +0530

    adding addition python file on dev

---
+++ addition.py
@@ -0,0 +1,13 @@
+# Taking input from user
+a = int ( input ( "Enter a number ") )
+b = int ( input ( "Enter b number " ) )
+
+# Now adding two numbers and storing to other variable
+
+c = a + b
+
+# print the new variable 
+
+print ( c )
+
+
